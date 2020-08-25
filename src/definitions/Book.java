d1f79ddde46afffbf4de72797c00fb6cb52c51e1
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2020-08-25 14:22:49 +0530

    Book.java Initiallized the Varibles To store the name of book, it's Author And ISBN code of it

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,20 +6,15 @@
  * */
 package definitions;
 
+import javax.xml.namespace.QName;
 import java.util.Scanner;
 
 public class Book {
-    /*
-    THIS IS NOT A EXECUTION CLASS I'LL HAVE TO REMOVE IT AT THE END
-     */
-    public static void main(String[] args) {
 
-        System.out.println("PLEASE ENTER THE NAME OF BOOK YOU WANT:");
-        Scanner scannerobject = new Scanner(System.in);
-        String [] nameOfBooks = new String[100];
-        String [] nameofBooksAuthor = new String[100];
-
-
+    String nameOFBook;
+    String nameOfAuthor;
+//    since it Should be a of 13 Digits
+    String isbnNumberOfBook;
 
     }
-}
+
